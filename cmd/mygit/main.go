149b55a0c6f8a8ce0b0020f396e6f50ca57ebc80
semihbkgr <buyukgungorsemih@gmail.com>
2024-03-16 23:53:13 +0300

    write blob object

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
 	"errors"
 	"fmt"
 	"io"
@@ -49,9 +50,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		dir := objectHash[0:2]
-		file := objectHash[2:]
-
+		dir, file := splitDirFile(objectHash)
 		b, err := os.ReadFile(filepath.Join(".git/objects", dir, file))
 		if err != nil {
 			fmt.Printf("error on reading object file: %v", err)
@@ -71,6 +70,53 @@ func main() {
 		}
 
 		fmt.Print(content)
+	case "hash-object":
+		if len(os.Args) != 4 {
+			fmt.Println("git cat-file -w <filename>")
+			os.Exit(1)
+		}
+
+		flag := os.Args[2]
+
+		if flag != "-w" {
+			fmt.Println("only -w flag is supported")
+			os.Exit(1)
+		}
+
+		filePath := os.Args[3]
+		content, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Printf("error on reading file: %v", err)
+			os.Exit(1)
+		}
+
+		blob := blobObject(content)
+		hash := hashHex(blob)
+		zippedBlob, err := zip(blob)
+		if err != nil {
+			fmt.Printf("error on zipping blob object: %v", err)
+			os.Exit(1)
+		}
+
+		dir, file := splitDirFile(hash)
+		if err := os.Mkdir(filepath.Join(".git/objects", dir), 0644); err != nil && !os.IsExist(err) {
+			fmt.Printf("error on creating object dir: %v", err)
+			os.Exit(1)
+		}
+		f, err := os.OpenFile(filepath.Join(".git/objects", dir, file), os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0644)
+		if err != nil {
+			fmt.Printf("error on opening object file: %v", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		_, err = io.CopyBuffer(f, bytes.NewReader(zippedBlob), make([]byte, 512))
+		if err != nil {
+			fmt.Printf("error on writing object file: %v", err)
+			os.Exit(1)
+		}
+
+		fmt.Print(hash)
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
@@ -94,10 +140,39 @@ func unzip(b []byte) ([]byte, error) {
 }
 
 func parseBlobContent(b []byte) (string, error) {
+	// blob <size>\x00<content>
 	i := bytes.IndexByte(b, 0)
 	if i < 0 {
 		return "", errors.New("cannot extract blob content")
 	}
 
 	return string(b[i+1:]), nil
+}
+
+func zip(b []byte) ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	w := zlib.NewWriter(buf)
+	_, err := w.Write(b)
+	if err != nil {
+		return nil, err
+	}
+
+	w.Close()
+	return buf.Bytes(), nil
+}
+
+func hashHex(b []byte) string {
+	h := sha1.Sum(b)
+	// hex dump
+	return fmt.Sprintf("%x", h)
+}
+
+func blobObject(b []byte) []byte {
+	header := fmt.Sprintf("blob %d", len(b))
+	headerBytes := append([]byte(header), 0)
+	return append(headerBytes, b...)
+}
+
+func splitDirFile(hex string) (string, string) {
+	return hex[:2], hex[2:]
 }
