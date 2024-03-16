cd27b8dda0ce9da69232fec49af2b32af3f2d81f
semihbkgr <buyukgungorsemih@gmail.com>
2024-03-16 22:54:15 +0300

    git cat-file subcommand

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -25,9 +29,59 @@ func main() {
 		}
 
 		fmt.Println("Initialized git directory")
+	case "cat-file": // git cat-file -p <object-SHA1>
+		if len(os.Args) != 4 {
+			fmt.Println("git cat-file -p <object-SHA1>")
+			os.Exit(1)
+		}
 
+		flag := os.Args[2]
+
+		if flag != "-p" {
+			fmt.Println("only -p flag is supported")
+			os.Exit(1)
+		}
+
+		objectHash := os.Args[3]
+		if len(objectHash) != 40 {
+			fmt.Println("len of object hash must be 40")
+			os.Exit(1)
+		}
+
+		dir := objectHash[0:2]
+		file := objectHash[2:]
+
+		b, err := os.ReadFile(filepath.Join(".git/objects", dir, file))
+		if err != nil {
+			fmt.Printf("error on reading object file: %v", err)
+			os.Exit(1)
+		}
+
+		content, err := unzip(b)
+		if err != nil {
+			fmt.Printf("error on unzipping object file: %v", err)
+			os.Exit(1)
+		}
+
+		fmt.Print(string(content))
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
 	}
+}
+
+func unzip(b []byte) ([]byte, error) {
+	r := bytes.NewReader(b)
+	zr, err := zlib.NewReader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	buf := bytes.NewBuffer(nil)
+	_, err = io.CopyBuffer(buf, zr, make([]byte, 512))
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
